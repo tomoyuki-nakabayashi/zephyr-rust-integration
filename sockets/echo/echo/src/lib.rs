eee3e840ba02e97ef0346d1c53f1d27112f7baae
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2019-02-25 09:17:52 +0900

    sample: [socket echo] Replace all codes with Rust!

--- sockets/echo/echo/src/lib.rs
+++ sockets/echo/echo/src/lib.rs
@@ -8,11 +8,21 @@ const PORT: u16 = 4242u16;
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-    unsafe {
-        zephyr::printf(
-            b"Hello from %s\0".as_ptr() as *const cty::c_char,
-            b"Rust\n\0".as_ptr(),
-        );
+    let server_desc = socket_init();
+    unsafe { zephyr::printf("TCP echo server waits for a connection on port %u...\n\0".as_ptr()  as *const cty::c_char,
+        PORT as cty::c_uint); }
+
+    loop {
+        let client_desc = establish_connection(server_desc);
+
+        loop {
+            if echo(client_desc) < 0 {
+                break;
+            }
+        }
+
+        unsafe { zephyr::_impl_zsock_close(client_desc) };
+        unsafe { zephyr::printf("Connection closed\n\0".as_ptr()  as *const cty::c_char); }
     }
 }
 
@@ -54,7 +64,12 @@ pub extern "C" fn socket_init() -> i32 {
         panic!();
     }
 
-    unsafe { zephyr::printf(b"Socket descriptor is %d\n\0".as_ptr() as *const cty::c_char, server); }
+    unsafe {
+        zephyr::printf(
+            b"Socket descriptor is %d\n\0".as_ptr() as *const cty::c_char,
+            server,
+        );
+    }
     server
 }
 
@@ -64,32 +79,75 @@ pub extern "C" fn socket_init() -> i32 {
 /// - return: client socket file descriptor.
 #[no_mangle]
 pub extern "C" fn establish_connection(server_dsc: cty::c_int) -> i32 {
-    unsafe { zephyr::printf(b"wait for client on socket #%d\n\0".as_ptr() as *const cty::c_char, server_dsc) };
-    let mut client_addr = zephyr::sockaddr { sa_family: 0, data: [0; 6] };
-    let mut client_addr_len = size_of::<zephyr::sockaddr>();
-    let client_dsc = unsafe {
-        zephyr::_impl_zsock_accept(server_dsc, &mut client_addr, &mut client_addr_len)
+    unsafe {
+        zephyr::printf(
+            b"wait for client on socket #%d\n\0".as_ptr() as *const cty::c_char,
+            server_dsc,
+        )
     };
+    let mut client_addr = zephyr::sockaddr {
+        sa_family: 0,
+        data: [0; 6],
+    };
+    let mut client_addr_len = size_of::<zephyr::sockaddr>();
+    let client_dsc =
+        unsafe { zephyr::_impl_zsock_accept(server_dsc, &mut client_addr, &mut client_addr_len) };
 
     if client_dsc < 0 {
         unsafe { zephyr::printf(b"error: accept\n\0".as_ptr() as *const cty::c_char) };
         panic!();
     }
 
-    let addr = {
-        let ip_addr = &client_addr.data[2..];
-
-        unsafe { zephyr::printf(b"Connection from %d.%d.%d.%d\n\0".as_ptr() as *const cty::c_char,
-            ip_addr[0] as cty::c_uint, ip_addr[1] as cty::c_uint, ip_addr[2] as cty::c_uint, ip_addr[3] as cty::c_uint); }
-    };
+    let ip_addr = &client_addr.data[2..];
+    unsafe {
+        zephyr::printf(
+            b"Connection from %d.%d.%d.%d\n\0".as_ptr() as *const cty::c_char,
+            u32::from(ip_addr[0]),
+            u32::from(ip_addr[1]),
+            u32::from(ip_addr[2]),
+            u32::from(ip_addr[3]),
+        );
+    }
 
     client_dsc
+}
+
+#[no_mangle]
+pub extern "C" fn echo(client_desc: cty::c_int) -> cty::ssize_t {
+    let mut buf: [cty::c_char; 128] = [0; 128];
+    let len = unsafe {
+        zephyr::_impl_zsock_recvfrom(
+            client_desc,
+            buf.as_mut_ptr() as *mut cty::c_void,
+            buf.len(),
+            0,
+            core::ptr::null_mut(),
+            core::ptr::null_mut(),
+        )
+    };
+
+    if len < 0 {
+        return len;
+    }
+
+    unsafe {
+        zephyr::_impl_zsock_sendto(
+            client_desc,
+            buf.as_mut_ptr() as *mut cty::c_void,
+            len as usize,
+            0,
+            core::ptr::null_mut(),
+            0,
+        )
+    }
 }
 
 use core::panic::PanicInfo;
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
-    unsafe { zephyr::printf(b"panic!".as_ptr() as *const cty::c_char); }
+    unsafe {
+        zephyr::printf(b"panic!".as_ptr() as *const cty::c_char);
+    }
     loop {}
 }
--- sockets/echo/src/socket_echo.c
+++ sockets/echo/src/socket_echo.c
@@ -28,39 +28,4 @@
 int main(void)
 {
 	rust_main();
-	int serv = socket_init();
-
-	printf("Single-threaded TCP echo server waits for a connection on port %d...\n", PORT);
-
-	while (1) {
-		int client = establish_connection(serv);
-
-		while (1) {
-			char buf[128], *p;
-			int len = recv(client, buf, sizeof(buf), 0);
-			int out_len;
-
-			if (len <= 0) {
-				if (len < 0) {
-					printf("error: recv: %d\n", errno);
-				}
-				break;
-			}
-
-			p = buf;
-			do {
-				out_len = send(client, p, len, 0);
-				if (out_len < 0) {
-					printf("error: send: %d\n", errno);
-					goto error;
-				}
-				p += out_len;
-				len -= out_len;
-			} while (len);
-		}
-
-error:
-		close(client);
-		printf("Connection closed\n");
-	}
 }
