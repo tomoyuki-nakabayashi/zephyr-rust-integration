f660d4c3db3888615199645ec6f911f4989826cf
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2019-02-26 16:33:50 +0900

    refactor: [socket echo] Remove unnecessary functions.

--- sockets/echo/echo/src/lib.rs
+++ sockets/echo/echo/src/lib.rs
@@ -1,55 +1,37 @@
 #![no_std]
 
 use cty;
-use zephyr_ffi::socket::{self, RawFd, Errno, AddressFamily, InetAddr, Ipv4Addr, SockProtocol, SockType};
+use zephyr_ffi::socket::{self, Errno, AddressFamily, InetAddr, Ipv4Addr, SockProtocol, SockType};
 use zephyr_ffi::{print, println};
 
 const PORT: u16 = 4242u16;
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-    let server_desc = socket_init().expect("fail to initialize socket.");
+    // Create a new socket.
+    let server = socket::socket(AddressFamily::Inet, SockType::Stream, SockProtocol::Tcp)
+        .expect("fail to create new socket.");
+
+    // Bind and listen on the created socket.
+    let bind_addr = InetAddr::new(Ipv4Addr::any(), PORT);
+    socket::bind(server, &bind_addr).expect("fail to bind.");
+    socket::listen(server, 5).expect("fail to listen.");
+
     println!("TCP echo server waits for a connection on port {}...", PORT);
 
     loop {
-        let client_desc = establish_connection(server_desc);
+        println!("wait for client on socket #{}", server);
+        let client = socket::accept(server).expect("fail to accept");
 
-        loop {
-            if echo(client_desc).is_err() {
-                break;
-            }
-        }
+        // Echo, repeatedly until we encounter an error.
+        while let Ok(_) = echo(client) {}
 
-        socket::close(client_desc).expect("fail to close");
+        socket::close(client).expect("fail to close");
         println!("Connection closed");
     }
 }
 
-/// Initialize socket.
-/// Return the new socket file descriptor of server.
-#[no_mangle]
-pub extern "C" fn socket_init() -> Result<RawFd, Errno> {
-    let fd = socket::socket(AddressFamily::Inet, SockType::Stream, SockProtocol::Tcp)?;
-
-    let bind_addr = InetAddr::new(Ipv4Addr::any(), PORT);
-    socket::bind(fd, &bind_addr)?;
-    socket::listen(fd, 5)?;
-
-    Ok(fd)
-}
-
-/// Establish connection with a client.
-///
-/// - server_dsc: socket file descriptor of server.
-/// - return: client socket file descriptor.
-#[no_mangle]
-pub extern "C" fn establish_connection(server_dsc: cty::c_int) -> i32 {
-    println!("wait for client on socket #{}", server_dsc);
-    socket::accept(server_dsc).expect("fail to accept")
-}
-
-#[no_mangle]
-pub extern "C" fn echo(client_desc: cty::c_int) -> Result<isize, Errno> {
+fn echo(client_desc: cty::c_int) -> Result<isize, Errno> {
     let mut buf: [cty::c_char; 128] = [0; 128];
     let len = socket::recv(client_desc, &mut buf)?;
     let buf = &buf[0..len as usize];
